f2511c111a2a426e64d74622c2c24124b0c52998
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2022-12-08 15:23:00 +0530

    Create example.ps1

---
+++ pwsh/example.ps1
@@ -0,0 +1,6 @@
+Write-Host "Ran the Script" -ForegroundColor Yellow
+
+# take username as input
+$UserName = Read-Host "Enter your name"
+# print the username
+Write-Host "Hello $UserName" -ForegroundColor Green
